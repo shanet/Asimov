8e2fd5629f2befb5289a597cb971fd351001bdb6
Gage Ames <gra5028@psu.edu>
2013-02-07 16:38:35 -0500

    Initial client commit.

---
+++ src/client/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AsimovClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Roomba;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RoombaController roomba = new RoombaController("127.0.0.1", 5000);
+        }
+    }
+}
