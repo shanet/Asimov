e344f8c05696d210a56b4915d5042428ba40b39c
Gage Ames <gra5028@psu.edu>
2013-02-22 20:32:44 -0500

    Implemented initial Kinect interaction to center a person.

--- src/client/AsimovClient.cs
+++ src/client/AsimovClient.cs
@@ -6,27 +6,58 @@
 
 namespace AsimovClient
 {
+    using System;
+    using System.Threading;
     using Create;
     using Logging;
-    using System;
+    using Microsoft.Kinect.Toolkit;
+    using Sensing;
 
     public class AsimovClient
     {
+        private static ManualResetEvent endEvent;
+
+        private static ICreateController roomba;
+
+        private static KinectSensorChooser sensorChooser;
+
+        private static PersonLocator personLocator;
+
         public static void Main(string[] args)
         {
             try
             {
-                ICreateController roomba = new ConsoleCreateController();
+                roomba = new ConsoleCreateController();                
+                endEvent = new ManualResetEvent(false);
+                sensorChooser = new KinectSensorChooser();
+                personLocator = new PersonLocator(sensorChooser);
 
-                roomba.PowerOn();
-                roomba.Drive(1, 2);
-                roomba.PowerOff();
+                // Subscribe to events that we need to handle
+                personLocator.OnPersonNotCentered += OnPersonNotCentered;
+                personLocator.OnPersonCentered += OnPersonCentered;
+
+                // Start the sensor!
+                //TODO: Surround in a try catch?
+                sensorChooser.Kinect.Start();
+
+                endEvent.WaitOne();
             }
             catch (Exception e)
             {
                 Console.Error.WriteLine("FATAL ERROR: {0}", e);
                 AsimovLog.WriteLine("FATAL ERROR: {0}", e);
             }
+        }
+
+        private static void OnPersonCentered(object sender)
+        {
+            roomba.Stop();
+        }
+
+        private static void OnPersonNotCentered(object sender, double angle)
+        {
+            //TODO: Turn a certain direction rather than a specific angle
+            roomba.Turn(1, 2, (int)angle);
         }
     }
 }
---
+++ src/client/Sensing/PersonLocator.cs
@@ -0,0 +1,95 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="PersonLocator.cs" company="Gage Ames">
+//     Copyright (c) Gage Ames.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace AsimovClient.Sensing
+{
+    using System;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+
+    public class PersonLocator
+    {
+        public const double CenteredTolerance = 3.0;
+
+        private KinectSensor kinect;
+
+        private bool isPersonNotCenteredInvoked;
+
+        public PersonLocator(KinectSensorChooser sensorChooser)
+        {
+            this.isPersonNotCenteredInvoked = false;
+
+            sensorChooser.KinectChanged += this.KinectSensorChanged;
+            sensorChooser.Start();
+
+            // Turn on the skeleton stream to receive skeleton frames
+            this.kinect.SkeletonStream.Enable();
+
+            // Add an event handler to be called whenever there is new color frame data
+            this.kinect.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+        }
+
+        public delegate void OnPersonCenteredHandler(object sender);
+
+        public delegate void OnPersonNotCenteredHandler(object sender, double angle);
+
+        public event OnPersonNotCenteredHandler OnPersonNotCentered;
+
+        public event OnPersonCenteredHandler OnPersonCentered;
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            // Save all of the skeletons
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+
+            // If we have skeletons to process
+            if (skeletons.Length != 0)
+            {
+                // Process each skeleton
+                foreach (Skeleton skeleton in skeletons)
+                {
+                    if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        double angle = Math.Atan2(skeleton.Position.X, skeleton.Position.Z) * 180 / Math.PI;
+
+                        // Determine if we need to center the skeleton we see
+                        if (angle < -CenteredTolerance || angle > CenteredTolerance)
+                        {
+                            this.isPersonNotCenteredInvoked = true;
+                            this.OnPersonNotCentered.Invoke(this, angle);
+                        }
+                        else if (this.isPersonNotCenteredInvoked == true)
+                        {
+                            this.OnPersonCentered.Invoke(this);
+                            this.isPersonNotCenteredInvoked = false;
+                        }
+                    }
+                    else if (skeleton.TrackingState == SkeletonTrackingState.PositionOnly)
+                    {
+                        //TODO
+                    }
+                }
+            }
+        }
+
+        private void KinectSensorChanged(object sender, KinectChangedEventArgs args)
+        {
+            if (args.NewSensor != null)
+            {
+                this.kinect = args.NewSensor;
+            }
+        }
+    }
+}
