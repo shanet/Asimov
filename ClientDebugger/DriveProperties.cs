136f956c5a0d79bff116a47a56a750d42014a555
Gage Ames <gra5028@psu.edu>
2013-03-04 14:00:24 -0500

    Initial ClientDebugger Commit: Drive GUI

---
+++ ClientDebugger/DriveProperties.cs
@@ -0,0 +1,35 @@
+﻿namespace ClientDebugger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DriveProperties
+    {
+        public bool IsIndefinate { get; set; }
+
+        public bool IsTime { get; set; }
+
+        public bool IsDistance { get; set; }
+
+        public bool IsAngle { get; set; }
+
+        public bool IsRadius { get; set; }
+
+        public bool IsStraight { get; set; }
+
+        public bool IsSpin { get; set; }
+
+        public double Velocity { get; set; }
+
+        public double Time { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Angle { get; set; }
+
+        public double Radius { get; set; }
+    }
+}
---
+++ ClientDebugger/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientDebugger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AngleRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.AngleTextBox != null)
+            {
+                this.AngleTextBox.IsEnabled = true;
+            }
+        }
+
+        private void AngleRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (this.AngleTextBox != null)
+            {
+                this.AngleTextBox.IsEnabled = false;
+            }
+        }
+
+        private void TimeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.TimeTextBox != null)
+            {
+                this.TimeTextBox.IsEnabled = true;
+            }
+        }
+
+        private void TimeRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (this.TimeTextBox != null)
+            {
+                this.TimeTextBox.IsEnabled = false;
+            }
+        }
+
+        private void DistanceRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.DistanceTextBox != null)
+            {
+                this.DistanceTextBox.IsEnabled = true;
+            }
+        }
+
+        private void DistanceRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (this.DistanceTextBox != null)
+            {
+                this.DistanceTextBox.IsEnabled = false;
+            }
+        }
+
+        private void RadiusRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.RadiusTextBox != null)
+            {
+                this.RadiusTextBox.IsEnabled = true;
+            }
+        }
+
+        private void RadiusRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (this.RadiusTextBox != null)
+            {
+                this.RadiusTextBox.IsEnabled = false;
+            }
+        }
+    }
+}
